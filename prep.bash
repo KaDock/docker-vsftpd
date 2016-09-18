8540d5b980a6c25a470b542a00388de684c5f4c8
Kalle Møller <github.com@k-moeller.dk>
2016-09-18 21:01:21 +0200

    Update prep.bash

--- prep.bash
+++ prep.bash
@@ -2,7 +2,7 @@
 FTP_USERNAME=${FTP_USERNAME:-kat}
 FTP_PASSWORD=${FTP_PASSWORD:-kat}
 FTP_USERID=${FTP_USERID:-1000}
-useradd -u ${FTP_USERID} ${FTP_USERNAME}
+useradd -m -u ${FTP_USERID} ${FTP_USERNAME}
 echo "${FTP_USERNAME}:${FTP_PASSWORD}" | chpasswd
 mkdir -p /data/ftp
 chown -R ${FTP_USERNAME}:${FTP_USERNAME} /data/ftp
