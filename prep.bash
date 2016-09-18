8a8a040c57a696fda438104bf3dd24b10e08b63e
Kalle Møller <github.com@k-moeller.dk>
2016-09-18 20:44:36 +0200

    Update prep.bash

--- prep.bash
+++ prep.bash
@@ -3,7 +3,7 @@ FTP_USERNAME=${FTP_USERNAME:-kat}
 FTP_PASSWORD=${FTP_PASSWORD:-kat}
 FTP_USERID=${FTP_USERID:-1000}
 useradd -u ${FTP_USERID} ${FTP_USERNAME}
-echo “${FTP_USERNAME}:${FTP_PASSWORD}” | chpasswd
+echo "${FTP_USERNAME}:${FTP_PASSWORD}" | chpasswd
 mkdir -p /data/ftp
 chown -R ${FTP_USERNAME}:${FTP_USERNAME} /data/ftp
 /usr/sbin/vsftpd
