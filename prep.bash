498008476979ecc67617d704d8cfddc179e5c894
KalleDK <git@k-moeller.dk>
2016-11-16 17:10:12 +0100

    S6

--- prep.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-FTP_USERNAME=${FTP_USERNAME:-kat}
-FTP_PASSWORD=${FTP_PASSWORD:-kat}
-FTP_USERID=${FTP_USERID:-1000}
-useradd -m -u ${FTP_USERID} ${FTP_USERNAME}
-echo "${FTP_USERNAME}:${FTP_PASSWORD}" | chpasswd
-mkdir -p /data/ftp
-chown -R ${FTP_USERNAME}:${FTP_USERNAME} /data/ftp
-/usr/sbin/vsftpd
